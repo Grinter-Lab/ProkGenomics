614a65c06c4b5a340da22322e4a9b295a133234c
Laura Perlaza Jiménez <lauraperlaza@gmail.com>
2024-02-28 23:30:19 +1100

    report_update_2

--- scripts/db_checkv.sh
+++ scripts/db_checkv.sh
@@ -1,5 +1,9 @@
 rm -r checkv-db*
-curl -C --retry 5 --retry-all-errors https://portal.nersc.gov/CheckV/checkv-db-v0.6-full.tar.gz
+
+while true;do
+    wget -T 15 -c https://portal.nersc.gov/CheckV/checkv-db-v0.6-full.tar.gz && break
+done
+
 tar -zxvf checkv-db-v0.6-full.tar.gz  
 cd checkv-db-v0.6
 diamond makedb --in checkv_reps.faa --db checkv_reps
